3b9a7eec15e3b3d79faeece272bda7fce4c1c4d3
emibcn <emi@algorismia.com>
2021-06-10 22:55:55 +0200

    Package: Fix and simplify building and testing environments: - Add `source` to package - Use `microbundle-crl` (which uses `rollup`) instead of `webpack` - Simplify jest options - Move babel options to a file, only used in tests - Unify and simplify testing scripts - Add linting test (not used yet in pipe)

---
+++ babelrc.test.js
@@ -0,0 +1,23 @@
+module.exports = {
+  "presets": [
+    [
+      "@babel/preset-env",
+      {targets: {node: "current"}}
+    ]
+  ],
+  "plugins": [
+    "@babel/plugin-transform-runtime",
+    [
+      "@babel/plugin-proposal-decorators",
+      {
+        "legacy": true
+      }
+    ],
+    [
+      "@babel/plugin-proposal-class-properties",
+      {
+        "loose": true
+      }
+    ]
+  ]
+}
